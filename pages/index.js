e59477389b9e85d369d81df2faeeb5aa9118a853
elwyn-de-neve <elwyn_de_neve@hotmail.com>
2022-10-16 16:00:24 +0200

    next-js contentful

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,10 @@ import { createClient } from "contentful";
 
 export async function getStaticProps() {
     const client = createClient( {
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_KEY
+        // space: process.env.CONTENTFUL_SPACE_ID,
+        // accessToken: process.env.CONTENTFUL_ACCESS_KEY
+        space: 'b34x3vlpckl6',
+        accessToken: 'h0ak6nsKoU7vVG-OnnXXT2GwVb2fslUNzW2t-ZrcV3U'
     } )
 
     const response = await client.getEntries( { content_type: 'ninjas' } )
@@ -29,7 +31,8 @@ export default function Home( { ninjas } ) {
             </Head>
             <div>
                 <h1 key={ ninjas[0].sys.id } className={ styles.title }>{ ninjas[0].fields.title }</h1>
-                {/*<p key={ ninjas[0].sys.id } className={ styles.text }>{ ninjas[0].fields.content }</p>*/}
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consectetur consequatur dolorum ducimus facere impedit itaque labore laboriosam maxime mollitia nihil nisi nostrum ratione repellat, reprehenderit saepe tempora, tempore velit!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet beatae, delectus dolore eum labore neque nesciunt quaerat suscipit temporibus!</p>
                 <Link href="/ninjas"><a className={ styles.btn }>See Ninja Listing</a></Link>
             </div>
         </>
